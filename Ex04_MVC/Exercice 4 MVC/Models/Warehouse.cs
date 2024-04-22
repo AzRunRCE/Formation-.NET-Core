a8037f02cc1ed2d6d61e47745ea72b9c046f1a18
AzRun <quentin.martinez@outlook.com>
2024-04-22 10:31:22 +0200

    Add files via upload

---
+++ Ex04_MVC/Exercice 4 MVC/Models/Warehouse.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercice_4_MVC.Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public int PostalCode { get; set; }
+    }
+}
---
+++ Ex04_MVC/README.md
@@ -0,0 +1,2 @@
+# fortil exercice 4 wms mvc
+Look at README.html for statement of exercise
